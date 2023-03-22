14db163a42ada150db3a7d87a39a6b8a1b8d672e
sosuisen <hidekazu.kubota@gmail.com>
2023-03-22 18:11:14 +0900

    feat: add kadai-hatten

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -1,8 +1,10 @@
 package com.example;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -10,14 +12,31 @@ public class MyApp extends Application {
 
 	@Override
 	public void start(Stage stage) {
-        var button = new Button("Hello");
-        button.setOnAction(e->{
-        	System.out.println("Hello, world");    
+		var label = new Label("Hello, world");
+		
+		var redBtn = new Button("赤");
+        redBtn.setOnAction(e->{
+        	label.setStyle("-fx-text-fill: red");
+        });
+		var blueBtn = new Button("青");
+        blueBtn.setOnAction(e->{
+        	label.setStyle("-fx-text-fill: blue");
+        });
+		var greenBtn = new Button("緑");
+        greenBtn.setOnAction(e->{
+        	label.setStyle("-fx-text-fill: green");
         });
 
-        var bPane = new BorderPane(button);
+        var bPane = new BorderPane();
+        bPane.setTop(label);
+        bPane.setLeft(redBtn);
+        bPane.setCenter(blueBtn);
+        bPane.setRight(greenBtn);
+        BorderPane.setAlignment(redBtn, Pos.TOP_CENTER);
+        BorderPane.setAlignment(blueBtn, Pos.TOP_CENTER);
+        BorderPane.setAlignment(greenBtn, Pos.TOP_CENTER);
 
-        var scene = new Scene(bPane, 320, 240);
+        var scene = new Scene(bPane, 160, 50);
         stage.setScene(scene);
         stage.show();
 	}
